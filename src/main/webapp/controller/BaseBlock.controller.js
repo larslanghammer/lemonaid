588fa71718a360b6b0f4b4676f82d77eafc5efd4
Lars Langhammer <l.langhammer@sap.com>
2017-06-14 10:09:08 +0200

    Some pretty printing

--- src/main/webapp/controller/BaseBlock.controller.js
+++ src/main/webapp/controller/BaseBlock.controller.js
@@ -3,60 +3,62 @@
 sap.ui.define([
     "com/sap/mentors/lemonaid/controller/BaseController",
     "com/sap/mentors/lemonaid/util/formatters"
-], function(BaseController, formatters) {
+], function (BaseController, formatters) {
     "use strict";
 
     return BaseController.extend("com.sap.mentors.lemonaid.controller.BaseBlock", {
 
-		formatters: formatters,
-		        onInit: function() {
-        	 var oEventBus = sap.ui.getCore().getEventBus();
-			oEventBus.subscribe("MyChannel", "doStuff", this.handleDoStuff, this);
-			oEventBus.subscribe("MyChannel", "doStuff2", this.handleDoStuff2, this);
-},
+        formatters: formatters,
+        onInit: function () {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.subscribe("MyChannel", "doStuff", this.handleDoStuff, this);
+            oEventBus.subscribe("MyChannel", "doStuff2", this.handleDoStuff2, this);
+        },
 
-handleDoStuff : function (channel, event, data) {
-	var oEventBus = sap.ui.getCore().getEventBus();
-   var oView = this.getView();
-	var controlsArray = oView.getContent()[0]._aElements;
-	var oData = [];
-	var Counter = 0;
-	console.log(oView.getContent()[0]._aElements);
-	for(var i in controlsArray){
-		var controlsId = controlsArray[i].sId;
-		if(controlsId.includes("input")){
-		oData.push(controlsArray[i].getValue());
-		}else if(controlsId.includes("select")){
-		oData.push(	controlsArray[i].getSelectedKey());
-		}else if(controlsId.includes("switch")){
-		oData.push(controlsArray[i].getState());
-		}
-	}
-   oEventBus.publish("MyChannelAddition", "doStuff",{data:oData});
+        handleDoStuff: function (channel, event, data) {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            var oView = this.getView();
+            var controlsArray = oView.getContent()[0]._aElements;
+            var oData = [];
+            var Counter = 0;
+            console.log(oView.getContent()[0]._aElements);
+            for (var i in controlsArray) {
+                var controlsId = controlsArray[i].sId;
+                if (controlsId.includes("input")) {
+                    oData.push(controlsArray[i].getValue());
+                } else if (controlsId.includes("select")) {
+                    oData.push(controlsArray[i].getSelectedKey());
+                } else if (controlsId.includes("switch")) {
+                    oData.push(controlsArray[i].getState());
+                }
+            }
+            oEventBus.publish("MyChannelAddition", "doStuff", {
+                data: oData
+            });
 
-},
+        },
 
-handleDoStuff2 : function (channel, event, data) {
-	var oView = this.getView();
-	var controlsArray = oView.getContent()[0]._aElements;
-	console.log(oView.getContent()[0]._aElements);
-	for(var i in controlsArray){
-		var controlsId = controlsArray[i].sId;
-		if(controlsId.includes("input")){
-		controlsArray[i].unbindAggregation("value",true)
-		}else if(controlsId.includes("select")){
-			controlsArray[i].unbindAggregation("selectedKey",true)
-		}else if(controlsId.includes("switch")){
-			controlsArray[i].unbindAggregation("state",true)
-		}
-	}
-/*	var input = oView.byId("FullName")
-	if( input != undefined){
-		oView.byId("FullName").unbindAggregation("value",true);
-		oView.unbindAggregation("model",true);
-   }*/
+        handleDoStuff2: function (channel, event, data) {
+            var oView = this.getView();
+            var controlsArray = oView.getContent()[0]._aElements;
+            console.log(oView.getContent()[0]._aElements);
+            for (var i in controlsArray) {
+                var controlsId = controlsArray[i].sId;
+                if (controlsId.includes("input")) {
+                    controlsArray[i].unbindAggregation("value", true)
+                } else if (controlsId.includes("select")) {
+                    controlsArray[i].unbindAggregation("selectedKey", true)
+                } else if (controlsId.includes("switch")) {
+                    controlsArray[i].unbindAggregation("state", true)
+                }
+            }
+            /*	var input = oView.byId("FullName")
+            	if( input != undefined){
+            		oView.byId("FullName").unbindAggregation("value",true);
+            		oView.unbindAggregation("model",true);
+               }*/
 
-},
+        },
 
 
     });
--- src/main/webapp/controller/MentorAddition.controller.js
+++ src/main/webapp/controller/MentorAddition.controller.js
@@ -3,9 +3,9 @@
 sap.ui.define([
     "com/sap/mentors/lemonaid/controller/BaseController",
     "sap/ui/model/json/JSONModel",
-	"sap/ui/model/Filter",
-	"sap/m/MessageToast"
-], function(BaseController, JSONModel, Filter, MessageToast) {
+    "sap/ui/model/Filter",
+    "sap/m/MessageToast"
+], function (BaseController, JSONModel, Filter, MessageToast) {
     "use strict";
 
     return BaseController.extend("com.sap.mentors.lemonaid.controller.MentorAddition", {
@@ -18,30 +18,30 @@ sap.ui.define([
          * Called when the master list controller is instantiated. It sets up the event handling for the master/detail communication and other lifecycle tasks.
          * @public
          */
-        onInit: function() {
-        	var oEventBus = sap.ui.getCore().getEventBus();
-			oEventBus.subscribe("MyChannelAddition", "doStuff", this.handleDoStuff, this);
+        onInit: function () {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.subscribe("MyChannelAddition", "doStuff", this.handleDoStuff, this);
 
-			this.view      = this.getView();
-			this.component = this.getComponent();
-			this.model     = this.component.getModel();
-			this.router    = this.getRouter();
-			this.i18n      = this.component.getModel("i18n").getResourceBundle();
-			this.config    = this.component.getModel("config");
-			this.ui        = new JSONModel({
-        		//ServiceUrl : this.model.sServiceUrl,
-				isEditMode : true,
-				FullName : "Lars"
-        	});
-        	this.view.setModel(this.ui, "ui");
-        	console.log(this.getModel("ui"));
+            this.view = this.getView();
+            this.component = this.getComponent();
+            this.model = this.component.getModel();
+            this.router = this.getRouter();
+            this.i18n = this.component.getModel("i18n").getResourceBundle();
+            this.config = this.component.getModel("config");
+            this.ui = new JSONModel({
+                //ServiceUrl : this.model.sServiceUrl,
+                isEditMode: true,
+                FullName: "Lars"
+            });
+            this.view.setModel(this.ui, "ui");
+            console.log(this.getModel("ui"));
             this.router.getRoute("MentorAddition").attachMatched(this.onRouteMatched, this);
 
             // Remove sections/blocks that are not meant for a general audience
-            this.config._loaded.then(function() {
+            this.config._loaded.then(function () {
                 if (!this.config.getProperty("/IsProjectMember") && !this.config.getProperty("/IsMentor")) {
-                	this.byId("ObjectPageLayout").removeSection(this.view.getId() + "--Media");
-                	this.byId("PersonalInfo").removeBlock(this.view.getId() + "--BlockAddress");
+                    this.byId("ObjectPageLayout").removeSection(this.view.getId() + "--Media");
+                    this.byId("PersonalInfo").removeBlock(this.view.getId() + "--BlockAddress");
                 }
             }.bind(this));
 
@@ -51,53 +51,53 @@ sap.ui.define([
         /* event handlers                                              */
         /* =========================================================== */
 
-        onRouteMatched: function(oEvent) {
-           // this.sMentorId = oEvent.getParameter("arguments").Id;
+        onRouteMatched: function (oEvent) {
+            // this.sMentorId = oEvent.getParameter("arguments").Id;
             this.model.metadataLoaded().then(this.bindView.bind(this));
         },
 
-		/**
-		 *
-		 * @param {sap.ui.base.Event} oEvent - 'press' event of Edit button
-		 */
-		onEdit: function(oEvent) {
-			this.ui.setProperty("/isEditMode", true);
-		},
+        /**
+         *
+         * @param {sap.ui.base.Event} oEvent - 'press' event of Edit button
+         */
+        onEdit: function (oEvent) {
+            this.ui.setProperty("/isEditMode", true);
+        },
 
-		/**
-		 *
-		 * @param {sap.ui.base.Event} oEvent - 'press' event of Save button
-		 */
-		onSave: function(oEvent) {
-			   var oEventBus = sap.ui.getCore().getEventBus();
-			   oEventBus.publish("MyChannel", "doStuff");
-		//	console.log(this.model);
-		//	console.log(this.ui);
-		//	console.log(this.view);
-		/*	this.model.submitChanges({
-				success: function(oData) {
-					MessageToast.show(this.i18n.getText("profileSavedSuccesfully"));
-					this.ui.setProperty("/isEditMode", false);
-				}.bind(this),
-				error: function(oError) {
-					MessageToast.show(this.i18n.getText("profileSavedError"));
-				}.bind(this)
-			});*/
-		},
+        /**
+         *
+         * @param {sap.ui.base.Event} oEvent - 'press' event of Save button
+         */
+        onSave: function (oEvent) {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.publish("MyChannel", "doStuff");
+            //	console.log(this.model);
+            //	console.log(this.ui);
+            //	console.log(this.view);
+            /*	this.model.submitChanges({
+            		success: function(oData) {
+            			MessageToast.show(this.i18n.getText("profileSavedSuccesfully"));
+            			this.ui.setProperty("/isEditMode", false);
+            		}.bind(this),
+            		error: function(oError) {
+            			MessageToast.show(this.i18n.getText("profileSavedError"));
+            		}.bind(this)
+            	});*/
+        },
 
-		/**
-		 *
-		 * @param {sap.ui.base.Event} oEvent - 'press' event of Cancel button
-		 */
-		onCancel: function(oEvent) {
-			this.model.resetChanges();
-			this.ui.setProperty("/isEditMode", false);
-		},
+        /**
+         *
+         * @param {sap.ui.base.Event} oEvent - 'press' event of Cancel button
+         */
+        onCancel: function (oEvent) {
+            this.model.resetChanges();
+            this.ui.setProperty("/isEditMode", false);
+        },
 
-		bindView: function() {
-					   var oEventBus = sap.ui.getCore().getEventBus();
-			   oEventBus.publish("MyChannel", "doStuff2");
-		/*	this.sMentorId = "fba652ed-32d6-46ff-a53d-1103dde715f8";
+        bindView: function () {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.publish("MyChannel", "doStuff2");
+            /*	this.sMentorId = "fba652ed-32d6-46ff-a53d-1103dde715f8";
 			console.log(this.getModel().createKey("/Mentors", { Id: this.sMentorId }));
             this.view.bindElement({
                 path: this.getModel().createKey("/Mentors", { Id: this.sMentorId }),
@@ -112,9 +112,9 @@ sap.ui.define([
 
         },
 
-        onUpload: function(event) {
+        onUpload: function (event) {
             var that = this,
-			    imp = this.ui.getProperty("/import"),
+                imp = this.ui.getProperty("/import"),
                 errorCount = 0,
                 updateCount = 0,
                 newCount = 0,
@@ -124,52 +124,83 @@ sap.ui.define([
             this.model.detachRequestFailed(
                 this.component._oErrorHandler._requestFailedHandler,
                 this.component._oErrorHandler);
-            jQuery.each(imp.data, function(rowIdx, row) {
-            	if (!row.__skip) {
-					var object = {};
-					jQuery.each(row, function(fieldName, field) {
-						fieldName = fieldName.replace(/\s+/g, "");
-						if (that._isValidField(fieldName)) {
-							object[fieldName] = field;
-						}
-					});
-					delete object.longitude;
-					delete object.latitude;
-					if (object.ShirtNumber) { object.ShirtNumber = that._parseInteger(object.ShirtNumber); } else { delete object.ShirtNumber; }
-					if (object.HoursAvailable) { object.HoursAvailable = that._parseInteger(object.HoursAvailable); } else { delete object.HoursAvailable; }
-					if (object.InterestInMentorCommunicationStrategy) { object.InterestInMentorCommunicationStrategy = that._parseBoolean(object.InterestInMentorCommunicationStrategy); } else { delete object.InterestInMentorCommunicationStrategy; }
-					if (object.InterestInMentorManagementModel) { object.InterestInMentorManagementModel = that._parseBoolean(object.InterestInMentorManagementModel); } else { delete object.InterestInMentorManagementModel; }
-					if (object.InterestInMentorMix) { object.InterestInMentorMix = that._parseBoolean(object.InterestInMentorMix); } else { delete object.InterestInMentorMix; }
-					if (object.InterestInOtherIdeas) { object.InterestInOtherIdeas = that._parseBoolean(object.InterestInOtherIdeas); } else { delete object.InterestInOtherIdeas; }
-					if (object.TopicLeadInterest) { object.TopicLeadInterest = that._parseBoolean(object.TopicLeadInterest); } else { delete object.TopicLeadInterest; }
-					if (object.HoursAvailable) { object.HoursAvailable = that._parseInteger(object.HoursAvailable); } else { delete object.HoursAvailable; }
-                    requests.push(new Promise(function(resolve) {
-    					if (row.__new) {
-    	            		if (!object.Id) {
-    	            			object.Id = that.guidGenerator.generateGuid();
-    	            		}
-    					    for (var i in object) {
-    					        if (object[i].length === 0) {
-    					            delete object[i];
-    					        }
-    					    }
+            jQuery.each(imp.data, function (rowIdx, row) {
+                if (!row.__skip) {
+                    var object = {};
+                    jQuery.each(row, function (fieldName, field) {
+                        fieldName = fieldName.replace(/\s+/g, "");
+                        if (that._isValidField(fieldName)) {
+                            object[fieldName] = field;
+                        }
+                    });
+                    delete object.longitude;
+                    delete object.latitude;
+                    if (object.ShirtNumber) {
+                        object.ShirtNumber = that._parseInteger(object.ShirtNumber);
+                    } else {
+                        delete object.ShirtNumber;
+                    }
+                    if (object.HoursAvailable) {
+                        object.HoursAvailable = that._parseInteger(object.HoursAvailable);
+                    } else {
+                        delete object.HoursAvailable;
+                    }
+                    if (object.InterestInMentorCommunicationStrategy) {
+                        object.InterestInMentorCommunicationStrategy = that._parseBoolean(object.InterestInMentorCommunicationStrategy);
+                    } else {
+                        delete object.InterestInMentorCommunicationStrategy;
+                    }
+                    if (object.InterestInMentorManagementModel) {
+                        object.InterestInMentorManagementModel = that._parseBoolean(object.InterestInMentorManagementModel);
+                    } else {
+                        delete object.InterestInMentorManagementModel;
+                    }
+                    if (object.InterestInMentorMix) {
+                        object.InterestInMentorMix = that._parseBoolean(object.InterestInMentorMix);
+                    } else {
+                        delete object.InterestInMentorMix;
+                    }
+                    if (object.InterestInOtherIdeas) {
+                        object.InterestInOtherIdeas = that._parseBoolean(object.InterestInOtherIdeas);
+                    } else {
+                        delete object.InterestInOtherIdeas;
+                    }
+                    if (object.TopicLeadInterest) {
+                        object.TopicLeadInterest = that._parseBoolean(object.TopicLeadInterest);
+                    } else {
+                        delete object.TopicLeadInterest;
+                    }
+                    if (object.HoursAvailable) {
+                        object.HoursAvailable = that._parseInteger(object.HoursAvailable);
+                    } else {
+                        delete object.HoursAvailable;
+                    }
+                    requests.push(new Promise(function (resolve) {
+                        if (row.__new) {
+                            if (!object.Id) {
+                                object.Id = that.guidGenerator.generateGuid();
+                            }
+                            for (var i in object) {
+                                if (object[i].length === 0) {
+                                    delete object[i];
+                                }
+                            }
                             that.model.create(
                                 "/Mentors",
-                                object,
-                                {
-                                    success: function(data) {
+                                object, {
+                                    success: function (data) {
                                         newCount++;
                                         resolve();
                                     },
-                                    error: function(error) {
+                                    error: function (error) {
                                         imp.errors.push({
-        									title: that.i18n.getText("importCreateErrorTitle"),
-        									message: that.i18n.getText("importCreateError", [
-        										object.Id,
-        										object.fullName,
+                                            title: that.i18n.getText("importCreateErrorTitle"),
+                                            message: that.i18n.getText("importCreateError", [
+                                                object.Id,
+                                                object.fullName,
                                                 error.responseText
-        									])
-        								});
+                                            ])
+                                        });
                                         errorCount++;
                                         resolve();
                                     }
@@ -177,22 +208,23 @@ sap.ui.define([
                             );
                         } else {
                             that.model.update(
-                                that.model.createKey("/Mentors", { Id: object.Id }),
-                                object,
-                                {
-                                    success: function(data) {
+                                that.model.createKey("/Mentors", {
+                                    Id: object.Id
+                                }),
+                                object, {
+                                    success: function (data) {
                                         updateCount++;
                                         resolve();
                                     },
-                                    error: function(error) {
+                                    error: function (error) {
                                         imp.errors.push({
-        									title: that.i18n.getText("importUpdateErrorTitle"),
-        									message: that.i18n.getText("importUpdateError", [
-        										object.Id,
-        										object.fullName,
+                                            title: that.i18n.getText("importUpdateErrorTitle"),
+                                            message: that.i18n.getText("importUpdateError", [
+                                                object.Id,
+                                                object.fullName,
                                                 error.responseText
-        									])
-        								});
+                                            ])
+                                        });
                                         errorCount++;
                                         resolve();
                                     }
@@ -200,7 +232,7 @@ sap.ui.define([
                             );
                         }
                     }));
-                    Promise.all(requests).then(function() {
+                    Promise.all(requests).then(function () {
                         if (errorCount === 0) {
                             imp.errors.push({
                                 title: that.i18n.getText("importSuccessTitle"),
@@ -217,15 +249,15 @@ sap.ui.define([
                             that.component._oErrorHandler);
                         that.ui.setProperty("/import", imp);
                     });
-            	}
+                }
             });
-		},
+        },
 
-		handleDoStuff : function (channel, event, data) {
-		var oView = this.getView();
-		console.log(data);
+        handleDoStuff: function (channel, event, data) {
+            var oView = this.getView();
+            console.log(data);
 
-		}
+        }
 
         /* =========================================================== */
         /* begin: internal methods                                     */
