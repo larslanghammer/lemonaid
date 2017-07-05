b8b67c85fa1574dd9976bc4eb716f5a5ff6afce0
Lars Langhammer <l.langhammer@sap.com>
2017-07-05 11:57:45 +0200

    Mentor Deletion

--- src/main/webapp/controller/MentorDetails.controller.js
+++ src/main/webapp/controller/MentorDetails.controller.js
@@ -4,8 +4,10 @@ sap.ui.define([
     "com/sap/mentors/lemonaid/controller/BaseController",
     "sap/ui/model/json/JSONModel",
 	"sap/ui/model/Filter",
+	'sap/m/Button',
+	'sap/m/Dialog',
 	"sap/m/MessageToast"
-], function(BaseController, JSONModel, Filter, MessageToast) {
+], function(BaseController, JSONModel, Filter, MessageToast, Dialog, Button) {
     "use strict";
 
     return BaseController.extend("com.sap.mentors.lemonaid.controller.MentorDetails", {
@@ -19,7 +21,7 @@ sap.ui.define([
          * @public
          */
         onInit: function() {
-        	
+
 			this.view      = this.getView();
 			this.component = this.getComponent();
 			this.model     = this.component.getModel();
@@ -28,11 +30,11 @@ sap.ui.define([
 			this.config    = this.component.getModel("config");
 			this.ui        = new JSONModel({
         		ServiceUrl : this.model.sServiceUrl,
-				isEditMode : false
+				isEditMode : true//false
         	});
         	this.view.setModel(this.ui, "ui");
             this.router.getRoute("Mentor").attachMatched(this.onRouteMatched, this);
-            
+
             // Remove sections/blocks that are not meant for a general audience
             this.config._loaded.then(function() {
                 if (!this.config.getProperty("/IsProjectMember") && !this.config.getProperty("/IsMentor")) {
@@ -40,7 +42,7 @@ sap.ui.define([
                 	this.byId("PersonalInfo").removeBlock(this.view.getId() + "--BlockAddress");
                 }
             }.bind(this));
-            
+
         },
 
         /* =========================================================== */
@@ -95,6 +97,58 @@ sap.ui.define([
                 }
             });
             this.ui.setProperty("/UploadUrl", this.model.sServiceUrl + "/" + this.model.createKey("Mentors", {Id: this.sMentorId}) + "/Attachments");
+        },
+
+        onDelete: function(){
+        	var that = this;
+			var dialog = new Dialog({
+				title: 'Delete Profil',
+				type: 'Message',
+				content: new sap.m.Text({ text: 'Are you sure you want to delete this profil?' }),
+				beginButton: new sap.m.Button({
+					text: 'Delete',
+					type: 'Reject',
+					press: function(){
+						console.log(that.model.oData["Mentors('"+that.sMentorId+"')"])
+						     that.model.remove(
+                                "/Mentors('"+that.sMentorId+"')",
+                                //that.model.oData["Mentors('"+that.sMentorId+"')"],
+                                {
+                                    success: function(data) {
+                                        newCount++;
+                                        resolve();
+                                    },
+                                    error: function(error) {
+                                        imp.errors.push({
+        									title: that.i18n.getText("importCreateErrorTitle"),
+        									message: that.i18n.getText("importCreateError", [
+        										object.Id,
+        										object.fullName,
+                                                error.responseText
+        									])
+        								});
+                                        errorCount++;
+                                        resolve();
+                                    }
+                                }
+                            );
+        				sap.m.MessageToast.show('Profile deleted!');
+        				dialog.close();
+    				 }
+
+				}),
+				endButton: new sap.m.Button({
+					text: 'Cancel',
+					press: function () {
+						dialog.close();
+					}
+				}),
+				afterClose: function() {
+					dialog.destroy();
+				}
+			});
+
+			dialog.open();
         }
 
         /* =========================================================== */
