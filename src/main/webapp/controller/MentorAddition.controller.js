7261899b8fb0b1d0e103601236ce9a4375a22168
Lars Langhammer <l.langhammer@sap.com>
2017-07-10 11:44:39 +0200

    Too many Typos

--- src/main/webapp/controller/MentorAddition.controller.js
+++ src/main/webapp/controller/MentorAddition.controller.js
@@ -138,9 +138,9 @@ sap.ui.define([
 		    	//this.objectToUpload["Public"] = object.public;
 				this.objectToUpload["RegionId"] = object.region;
 				this.objectToUpload["RelationshipToSapId"] = object.relationshipToSap;
-                statusHelper = {};
-                statusHelper[__ref] = this.model.oData["MentorStatuses('"+object.status+"')"];
-				this.objectToUpload["MentorStatus"] = status;
+                var statusHelper = {};
+                statusHelper[__ref] ="MentorStatuses('"+object.status+"')"// this.model.oData["MentorStatuses('"+object.status+"')"];
+				this.objectToUpload["MentorStatus"] = statusHelper;
 			} else if (object.viewName.includes("Address")) {
 				this.objectToUpload["Address1"] = object.address1;
 				this.objectToUpload["Address2"] = object.address2;
