3eb381c2afb700c6f563ef951f252c8475bdc8fb
Lars Langhammer <l.langhammer@sap.com>
2017-07-28 14:47:37 +0200

    Removed unnecessary info from json

--- src/main/java/com/sap/mentors/lemonaid/odata/JPAEdmExtension.java
+++ src/main/java/com/sap/mentors/lemonaid/odata/JPAEdmExtension.java
@@ -60,8 +60,6 @@ public class JPAEdmExtension implements org.apache.olingo.odata2.jpa.processor.a
             +"Email1|"
             +"Email2|"
             +"SoftSkill1Id|SoftSkill2Id|SoftSkill3Id|SoftSkill4Id|SoftSkill5Id|SoftSkill6Id|"
-            +"AttachmentsPublic|"
-
 			+ "Longitude|Latitude";
 	public static final String PUBLIC_NAVPROPERTIES =
             "MentorStatus|RelationshipToSap|Region|Country|"
