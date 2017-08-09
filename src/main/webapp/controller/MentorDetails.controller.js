2a2710502c2f3be28821def552575660193cecf4
Lars Langhammer <l.langhammer@sap.com>
2017-08-09 12:53:16 +0200

    refresh true

--- src/main/webapp/controller/MentorDetails.controller.js
+++ src/main/webapp/controller/MentorDetails.controller.js
@@ -97,12 +97,14 @@ sap.ui.define([
                     this.ui.setProperty("/isEditMode", false);
                     console.log(this.model.oData["Mentors('" + this.sMentorId + "')"]);
                     this.bindView();
+                    this.model.refresh(true);
                 }.bind(this),
                 error: function (oError) {
                     this.busyDialog.close();
                     sap.m.MessageToast.show(this.i18n.getText("profileSavedError"));
                     console.log(this.model.oData["Mentors('" + this.sMentorId + "')"]);
                     this.bindView();
+                    this.model.refresh(true);
                 }.bind(this)
             });
            }else {
