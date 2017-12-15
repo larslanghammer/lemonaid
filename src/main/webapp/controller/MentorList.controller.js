c04af91df30cbf65f92c2be94891fb6578e42fd1
Lars Langhammer <l.langhammer@sap.com>
2017-12-15 10:30:09 +0100

    small fix

--- src/main/webapp/controller/MentorList.controller.js
+++ src/main/webapp/controller/MentorList.controller.js
@@ -71,7 +71,7 @@ sap.ui.define([
 					var upperCasedTerm = searchTerms[k].toUpperCase()
 					var lowerCasedTerm = searchTerms[k].toLowerCase()
 					for(var i = 0; i<splitTerm.length; i++){
-						camelCasedTerm = splitTerm[i][0].toUpperCase() + splitTerm.substr(1).toLowerCase()+" "
+						camelCasedTerm = splitTerm[i][0].toUpperCase() + splitTerm[i].substr(1).toLowerCase()+" "
 					}
 					camelCasedTerm.trim
 
